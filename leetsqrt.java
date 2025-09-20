c2f83ec2ec0949fa181b05c420c89f7588aeb7f2
Ayush <raj309457@gmail.com>
2025-09-20 19:27:07 +0530

    leetSqrt

---
+++ leetsqrt.java
@@ -0,0 +1,27 @@
+public class leetsqrt {
+    public static int mySqrt(int x) {
+        int start = 1;
+        int end = x;
+        int ans = 0;
+        if (x==0||x==1) {
+            return x;
+        }
+        while (start<=end) {
+            int mid = (start+end)/2;
+            long square =(long) mid*mid;
+            if (square==x ) {
+                return mid;
+            }else if(square<x){
+                ans = mid;
+                start = mid+1;
+            }else if (square>x) {
+                end = mid-1;
+            }
+        }
+        return ans;
+
+    }
+    public static void main(String args[]){
+        System.out.println(mySqrt(24));
+    }
+}
