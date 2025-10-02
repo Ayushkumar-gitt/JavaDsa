656ffa06df3c09d227deca57751fb19e44ccfe85
Ayush <raj309457@gmail.com>
2025-10-02 20:59:04 +0530

    push

--- Array2D.java
+++ Array2D.java
@@ -2,26 +2,26 @@ import java.util.Arrays;
 
 public class Array2D {
     public static void main(String[] args) {
-        int arr[][] = {{10,20,30,40},
-        {15,25,35,45},
-        {28,29,37,49},
-        {33,34,38,50}};
+        int arr[][] = { { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 28, 29, 37, 49 },
+                { 33, 34, 38, 50 } };
         int target = 33;
         System.out.println(Arrays.toString(search2D(arr, target)));
     }
-    
-    public static int[] search2D(int arr[][],int target){
+
+    public static int[] search2D(int arr[][], int target) {
         int row = 0;
-        int col = arr.length-1;
-        while (row<arr.length&&col>=0) {
-            if (arr[row][col]==target) {
-                return new int[]{row,col};
-            } else if(arr[row][col]<target){
+        int col = arr.length - 1;
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == target) {
+                return new int[] { row, col };
+            } else if (arr[row][col] < target) {
                 row++;
-            } else{
+            } else {
                 col--;
             }
         }
-        return new int[]{-1,-1};
+        return new int[] { -1, -1 };
     }
 }
