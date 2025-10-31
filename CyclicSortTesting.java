065a9bf9c622aa66b65da8837974d889ca003b49
Ayush <raj309457@gmail.com>
2025-10-31 22:39:48 +0530

    Cyclic Sort Missing number

--- CyclicSortTesting.java
+++ CyclicSortTesting.java
@@ -2,11 +2,11 @@ public class CyclicSortTesting {
     public static int cyclicSort(int arr[]){
         int i = 0;
         while (i <=arr.length-1){
-            int idx = arr[i];
-            if (arr[i]<arr.length && arr[i]!=arr[idx]) {
+            int index = arr[i];
+            if (arr[i]<arr.length && arr[i]!=arr[index]) {
                 int temp = arr[i];
-                arr[i] = arr[idx];
-                arr[idx] = temp;
+                arr[i] = arr[index];
+                arr[index] = temp;
             }else {
                 i++;
             }
@@ -22,7 +22,7 @@ public class CyclicSortTesting {
         return arr.length;
     }
     public static void main(String[] args) {
-        int arr[] = {9,6,4,2,3,5,7,0,1};
+        int arr[] = {9,6,4,2,3,5,7,8,1};
 
         System.out.println("Missing element : "+ cyclicSort(arr));
     }
