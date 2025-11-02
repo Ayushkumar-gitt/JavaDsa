7c758c5197c728c12c856dbea19437be27a8d813
Ayush <raj309457@gmail.com>
2025-11-02 22:03:36 +0530

    MultiMissingSNo

--- reverseArray.java
+++ reverseArray.java
@@ -2,7 +2,7 @@ public class reverseArray {
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
         for(int i = arr.length;i>=1;i--){
-            System.out.print(i);
+            System.out.print(i +" ");
         }
     }
 }
