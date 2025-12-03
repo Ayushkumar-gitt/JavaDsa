555bc0873dfd6265ae2f43ede8608b4d61a18a8b
Ayush <raj309457@gmail.com>
2025-12-03 13:31:05 +0530

    Defanging_IP leetcode

---
+++ Defanging_IP.java
@@ -0,0 +1,10 @@
+public class Defanging_IP {
+    public static void main(String[] args) {
+        String address = "1.1.1.1";
+        Defanging_IP_Strings(address);
+    }
+    public static String Defanging_IP_Strings(String address){
+        String address1 = address.replace(".", "[.]");
+        return address1;
+    }
+}
