c4ef54614538ceff236b4e02a987259d445a59e3
Ayush kumar <raj309457@gmail.com>
2026-01-16 20:30:49 +0530

    search in rotated array

---
+++ Leetcode33.java
@@ -0,0 +1,39 @@
+public class Leetcode33 {
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+
+        System.out.println(search(arr, 0));
+        // System.out.println(binarySearch(arr, 6, 0, arr.length));
+    }
+
+    public static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // Left half is sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
