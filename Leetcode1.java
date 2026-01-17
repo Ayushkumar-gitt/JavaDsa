8f1d79404d0ac3f7ce48d2efc15eb90fe4702f4a
Ayush kumar <raj309457@gmail.com>
2026-01-17 22:18:16 +0530

    Two sum

---
+++ Leetcode1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Leetcode1 {
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+    }
+    public int[] twoSum(int[] nums, int target) {
+        int arr[] = 
+        if (nums.length<=2) {
+            
+        }
+    }
+}
