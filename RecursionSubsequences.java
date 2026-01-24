e16aa2199f003d5c4533f3711bb2b72a8253bcfd
Ayush <raj309457@gmail.com>
2026-01-24 21:21:52 +0530

    Subsequence

--- RecursionSubsequences.java
+++ RecursionSubsequences.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class RecursionSubsequences {
     public static void main(String[] args) {
-        subsequences("", "abc");
+        // subsequences("", "abc");
+        System.out.println(subsequencesList("", "ahbgdc"));
     }
 
     public static void subsequences(String processed, String unprocessed) {
@@ -10,5 +13,18 @@ public class RecursionSubsequences {
         }
         subsequences(processed + unprocessed.charAt(0), unprocessed.substring(1));
         subsequences(processed, unprocessed.substring(1));
+    }
+    
+    public static ArrayList<String> subsequencesList(String processed, String unprocessed) {
+        if (unprocessed.length() == 0) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(processed);
+            return list;
+        }
+        ArrayList<String> right = subsequencesList(processed + unprocessed.charAt(0), unprocessed.substring(1));
+        ArrayList<String> left = subsequencesList(processed, unprocessed.substring(1));
+
+        right.addAll(left);
+        return right;
     }
 }
