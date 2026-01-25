49807531479d700de83ef2002db49c340cad13a8
Ayush <raj309457@gmail.com>
2026-01-25 16:47:12 +0530

    Leetcode Subsequence problems

--- RecursionSubsequences.java
+++ RecursionSubsequences.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 public class RecursionSubsequences {
     public static void main(String[] args) {
         // subsequences("", "abc");
-        System.out.println(subsequencesList("", "ahbgdc"));
+        // System.out.println(subsequencesList("", "ahbgdc"));
+        System.out.println(isSubsequence("axc", "abcde"));
+        String[] words = {"a","bb","acd","ace"};
+        numMatchingSubseq("abcde",words );
     }
 
     public static void subsequences(String processed, String unprocessed) {
@@ -26,5 +29,30 @@ public class RecursionSubsequences {
 
         right.addAll(left);
         return right;
+    }
+    public static boolean isSubsequence(String s, String t) {
+        int i = 0; 
+        int j = 0; 
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+    public static int numMatchingSubseq(String s, String[] words) {
+        int i = 0;
+        int count = 0;
+        
+        while (i<words.length) {
+            if (isSubsequence(words[i],s)==true) {
+                count++;
+            }
+            i++;
+        }
+        return count;
     }
 }
