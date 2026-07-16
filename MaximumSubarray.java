51d5831831749e4f3410cf08b1913f72d331b2aa
Ayush Kumar <120472968+Ayushkumar-gitt@users.noreply.github.com>
2026-07-16 17:59:48 +0530

    Longest substring

--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,17 +1,21 @@
+import java.util.HashMap;
+
 public class MaximumSubarray {
     public static void main() {
-        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(nums));
+        int[] nums = {-3, -2, -1};
+        String s = "abcabcbb";
+//        System.out.println(maxSubArray(nums));
+        System.out.println(lengthOfLongestSubstring(s));
     }
 
     public static int maxSubArray(int[] nums) {
-        int ans = nums[0];
+        int ans = nums[0]; // Even if there's a single element in the array , it will be returned as answer.
         int sum = 0;
         int start = 0;
         int end = 0;
         while (end < nums.length) {
             if (nums[start] < 0 || sum < 0) {
-                ans = nums[start];
+                ans = Math.max(ans, nums[start]);
                 sum = 0;
                 start++;
                 end = start;
@@ -24,5 +28,28 @@ public class MaximumSubarray {
             }
         }
         return ans;
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        int start = 0;
+        int end = 0;
+        int ans = 0;
+
+        while (end < s.length()) {
+            Character currChar = s.charAt(end);
+
+             if ((map.containsKey(currChar)) && map.get(currChar) >= start) {
+                start = map.get(currChar) + 1;
+            }else{
+                 map.put(currChar,end);
+                 if (ans<(end-start)){
+                     ans = end-start;
+                 }
+                 end++;
+             }
+        }
+        return ans+1;
     }
 }
